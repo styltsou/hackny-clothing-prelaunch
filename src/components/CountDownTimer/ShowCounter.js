4de4241715c3795b5e36d1c1c58c339e0c333496
styltsou <tsoumanis.stylianos@gmail.com>
2023-04-15 13:25:13 +0300

    fix bg image

--- src/components/CountDownTimer/ShowCounter.js
+++ src/components/CountDownTimer/ShowCounter.js
@@ -4,11 +4,8 @@ import styled from 'styled-components';
 import { frostedGlass } from '../../styles/glass';
 
 const Flex = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,28 +10,41 @@ import CountDownTimer from '../components/CountDownTimer';
 const LAUNCH_DATE = 'Mon May 01 2023 17:00:00 GMT+0300';
 
 const Main = styled.main`
+  display: grid;
   width: 100%;
   height: 100vh;
-  position: fixed;
-  top: 0;
-  left: 0;
   overflow: hidden;
   z-index: 0;
   background-color: #2d53a6;
+`;
+
+const BgWrapper = styled.div`
+  grid-area: 1/1;
+`;
+
+const ContentWrapper = styled.div`
+  grid-area: 1/1;
 `;
 
 const IndexPage = () => {
   return (
     <ThemeProvider theme={theme}>
       <Main>
+        {/* <BgWrapper> */}
         <StaticImage
           src="../images/bg-desktop.jpg"
-          alt="bacgkround"
+          alt=""
           placeholder="blurred"
-          // layout="fullWidth"
-          objectFit="cover"
+          layout="fullWidth"
+          style={{
+            gridArea: '1/1',
+          }}
+          // objectFit="cover"
         />
-        <CountDownTimer targetDate={LAUNCH_DATE} />
+        {/* </BgWrapper> */}
+        <ContentWrapper>
+          <CountDownTimer targetDate={LAUNCH_DATE} />
+        </ContentWrapper>
       </Main>
       <GlobalStyles />
     </ThemeProvider>
