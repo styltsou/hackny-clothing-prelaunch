0b7855cc0ddd4dfeddc0cb03b49242ba4d59baf9
styltsou <tsoumanis.stylianos@gmail.com>
2023-04-20 23:32:55 +0300

    add some code to interact with the sendgrid client

--- src/api/waitlist.js
+++ src/api/waitlist.js
@@ -1,21 +1,48 @@
 const client = require('@sendgrid/client');
 
-// client.setApiKey(process.env.SENDGRID_API_KEY);
+client.setApiKey(process.env.SENDGRID_API_KEY);
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   const { method, body } = req;
 
   if (method === 'POST') {
     const { email } = body;
 
-    res.status(200).json({
-      status: 'success',
-      email: 'some email',
-    });
+    const data = {
+      list_ids: [process.env.WAITLIST_SENDGRID_ID],
+      contacts: [{ email }],
+    };
+
+    try {
+      const sendigridResponse = await client.request({
+        url: `/v3/marketing/contacts`,
+        method: 'PUT',
+        body: data,
+      });
+
+      // ! Check respones here maybe??
+
+      res.status(200).json({
+        status: 'success',
+        email,
+      });
+    } catch (e) {
+      res.status(400).json({
+        status: 'fail',
+        message: e?.message || e,
+      });
+    }
   } else if (method === 'GET') {
+    const request = {
+      url: `/v3/marketing/lists`,
+      method: 'GET',
+    };
+
+    const response = await client.request(request);
+
     res.status(200).json({
       status: 'success',
-      message: 'Vercel supports gatsby api',
+      data: response,
     });
   } else {
     res.status(404).json({
