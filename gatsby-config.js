641ae1f6280d270c7a57dc0401cdae7e8bbb145a
Stylianos Tsoumanis <tsoumanis.stylianos@gmail.com>
2023-04-28 14:06:28 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-sitemap',
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        "trackingId": process.env.GA_TRACKING_ID
+      }
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         icon: 'src/images/favicon.png',
